4dd95a9990b315ccfee085104657039fe17abf12
Andreas Pardeike <andreas@pardeike.net>
2020-06-18 01:13:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Goosegun
+
+Shoots geeze. What did you expect?
+
+ENJOY
---
+++ Source/Tools.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Goosegun
+{
+	public class GooseBullet : Bullet
+	{
+		static readonly PawnKindDef gooseDef = PawnKindDef.Named("Goose");
+
+		protected override void Impact(Thing hitThing)
+		{
+			var faction = FactionUtility.DefaultFactionFrom(gooseDef.defaultFactionType);
+			var pawn = PawnGenerator.GeneratePawn(gooseDef, faction);
+			var goose = GenSpawn.Spawn(pawn, Position, Map, WipeMode.Vanish) as Pawn;
+			_ = goose.health.AddHediff(HediffDefOf.Scaria, null, null, null);
+			_ = goose.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent, null, false, false, null, false);
+
+			GenClamor.DoClamor(this, 2.1f, ClamorDefOf.Impact);
+			Destroy(DestroyMode.Vanish);
+		}
+	}
+}
